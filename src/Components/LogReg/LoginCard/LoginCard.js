586edac6f5b6bb1f3936b3a438cca80c622bd0f8
Prem Kumar <prmsrswt@gmail.com>
2019-06-20 17:05:36 +0530

    user login now works

--- src/Components/LogReg/LoginCard/LoginCard.js
+++ src/Components/LogReg/LoginCard/LoginCard.js
@@ -1,36 +1,55 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { Mutation } from "react-apollo";
+import { USR_LOGIN } from '../../../gqlDefs';
 import './LoginCard.css';
 
 const LoginCard = () => {
 	const [inputs, changeInputs] = useState({ email: '', password: '' });
-	const handleInput = ( event ) => {
-		const value = event.target.value;
-		const name = event.target.name;
-		changeInputs({ 
-			...inputs,
-			[name]: value
-		});
-	}
-	const handleSubmit = ( event ) => {
-		event.preventDefault();
-		const payload = {
-			email: inputs.email,
-			password: inputs.password
-		}
-		console.log('payload:', payload);
-	}
 	return (
-		<div className='loginCard'>
-			<h3>Welcome back!</h3>
-			<p>Login to access your dashboard</p>
-			<form className='loginForm' onSubmit={handleSubmit}>
-				<input className='loginInput' type='text' name='email' placeholder='E-mail' onChange={handleInput} required />
-				<input className='loginInput' type='password' name='password' placeholder='Password' onChange={handleInput} required />
-				<input type='submit' name='login' value='LOG IN' disabled={ false } />
-			</form>
-			<Link to='/reset'>Forgot your password?</Link>
-		</div>
+		<Mutation
+			mutation={USR_LOGIN}
+		>
+			{ ( login, { loading, data, error, client }) => {
+				const handleInput = ( event ) => {
+					const value = event.target.value;
+					const name = event.target.name;
+					changeInputs({ 
+						...inputs,
+						[name]: value
+					});
+				}
+				const handleSubmit = ( event ) => {
+					event.preventDefault();
+					const payload = {
+						email: inputs.email,
+						password: inputs.password
+					}
+					console.log('login payload:', payload);
+					login({ variables: payload });
+				}
+				if ( data ) {
+					window.localStorage.setItem('token', data.login);
+					console.log('login successful: token: ', data.login);
+					client.resetStore();
+					return (
+						<Redirect to="/" />
+					);
+				}
+				return (
+					<div className='loginCard'>
+						<h3>Welcome back!</h3>
+						<p>Login to access your dashboard</p>
+						<form className='loginForm' onSubmit={handleSubmit}>
+							<input className='loginInput' type='text' name='email' placeholder='E-mail' onChange={handleInput} required />
+							<input className='loginInput' type='password' name='password' placeholder='Password' onChange={handleInput} required />
+							<input type='submit' name='login' value='LOG IN' disabled={loading} />
+						</form>
+						<Link to='/reset'>Forgot your password?</Link>
+					</div>
+				)}
+			}
+		</Mutation>
 	);
 }
 
--- src/client.js
+++ src/client.js
@@ -1,12 +1,15 @@
 import ApolloClient from "apollo-boost";
 
-const token = localStorage.getItem('token');
-const headers = {
-	authorization: token ? `bearer ${token}` : ""
-}
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
-  headers: headers
+  request: async (operation) => {
+    const token = await localStorage.getItem('token');
+    operation.setContext({
+      headers: {
+        authorization: `bearer ${token}`
+      }
+    });
+  }
 });
 
 export default client;
