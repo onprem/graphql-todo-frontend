e7a501f8ff1b7d76823d0dc0e3dc30cb7598d669
Prem Kumar <prmsrswt@gmail.com>
2019-08-19 00:47:39 +0530

    migrated TodoInput to apollo-hooks

--- src/Components/Home/TodoInput.js
+++ src/Components/Home/TodoInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Mutation } from "react-apollo";
+import { useMutation } from '@apollo/react-hooks';
 import { ADD_TODO, GET_USER } from '../../gqlDefs';
 
 const TodoInput = ({ user }) => {
@@ -14,62 +14,66 @@ const TodoInput = ({ user }) => {
 			__typename: "Todo"
 		}])
 	}
-	return (
-		<Mutation
-			mutation={ADD_TODO}
-			update={( cache, { data: { addTodo } }) => {
-				//const { me } = cache.readQuery({ query: GET_USER });
-				cache.writeQuery({
-					query: GET_USER,
-					data: {
-						me: addTodo.user
-					}
-				});
-			}}
-			optimisticResponse={{addTodo: {
+	const [addTodo] = useMutation(ADD_TODO, {
+		update : (cache, { data: { addTodo } }) => {
+			//const { me } = cache.readQuery({ query: GET_USER });
+			cache.writeQuery({
+				query: GET_USER,
+				data: {
+					me: addTodo.user
+				}
+			});
+		},
+		optimisticResponse: {
+			addTodo: {
 				__typename: "addTodo",
 				code: "200",
 				success: true,
 				message: "todo successfully added",
 				user: opRes
-			}}}
-		>
-			{ addTodo => {
-				const handleChange = ( event ) => {
-					const value = event.target.value;
-					const name = event.target.name;
-					changeInputs({ 
-						...inputs,
-						[name]: value
-					});
-				}
-				const handleSubmit = ( event ) => {
-					//event.preventDefault();
-					if(!inputs.todo)
-						return;
-					const payload = {
-						todo: inputs.todo
-					}
-					console.log('add payload:', payload);
-					addTodo({ variables: payload });
-					changeInputs({
-						...inputs,
-						todo: ''
-					});
-				}
-				const onKeyDwn = ( event ) => {
-					if (event.key === 'Enter') {
-			        	event.preventDefault();
-			        	event.stopPropagation();
-			        	handleSubmit();
-			      }
-				}
-				return (
-					<input name='todo' type='text' placeholder='Add todo' className='addInput' onChange={handleChange} onKeyDown={onKeyDwn} value={inputs.todo} required />
-				);
-			}		
 			}
-		</Mutation>
+		}
+	});
+	const handleChange = ( event ) => {
+		const value = event.target.value;
+		const name = event.target.name;
+		changeInputs({ 
+			...inputs,
+			[name]: value
+		});
+	}
+	const handleSubmit = ( event ) => {
+		//event.preventDefault();
+		if(!inputs.todo)
+			return;
+		const payload = {
+			todo: inputs.todo
+		}
+		console.log('add payload:', payload);
+		addTodo({ variables: payload });
+		changeInputs({
+			...inputs,
+			todo: ''
+		});
+	}
+	const onKeyDwn = ( event ) => {
+		if (event.key === 'Enter') {
+			event.preventDefault();
+			event.stopPropagation();
+			handleSubmit();
+		}
+	}
+	return (
+		<input
+			name='todo'
+			type='text'
+			placeholder='Add todo'
+			className='addInput'
+			onChange={handleChange}
+			onKeyDown={onKeyDwn}
+			value={inputs.todo}
+			required
+		/>
 	);
 }
 
